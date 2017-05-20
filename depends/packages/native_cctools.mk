63a089041c8f87279470d31008433f33823b8511
dmrtsvetkov <dima121596@mail.ru>
2017-05-20 20:41:14 +0300

    Release
    Release

--- depends/packages/native_cctools.mk
+++ depends/packages/native_cctools.mk
@@ -13,10 +13,6 @@ $(package)_dyld_download_file=dyld-195.5.tar.gz
 $(package)_dyld_download_path=http://www.opensource.apple.com/tarballs/dyld
 $(package)_dyld_file_name=$($(package)_dyld_download_file)
 $(package)_dyld_sha256_hash=2cf0484c87cf79b606b351a7055a247dae84093ae92c747a74e0cde2c8c8f83c
-$(package)_clang_download_file=clang+llvm-3.2-x86-linux-ubuntu-12.04.tar.gz
-$(package)_clang_download_path=http://llvm.org/releases/3.2
-$(package)_clang_file_name=clang-llvm-3.2-x86-linux-ubuntu-12.04.tar.gz
-$(package)_clang_sha256_hash=b9d57a88f9514fa1f327a1a703756d0c1c960f4c58494a5bd80313245d13ffff
 
 define $(package)_fetch_cmds
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash)) && \
@@ -31,7 +27,6 @@ $(package)_cflags+=-m32
 $(package)_cxxflags+=-m32
 $(package)_cppflags+=-D__DARWIN_UNIX03 -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS
 $(package)_ldflags+=-m32 -Wl,-rpath=\\$$$$$$$$\$$$$$$$$ORIGIN/../lib
-$(package)_ldflags+=-L$$(native_cctools_extract_dir)/clang+llvm-3.2-x86-linux-ubuntu-12.04/lib
 endef
 define $(package)_extract_cmds
   ln -sf $($(package)_source) cctools2odcctools/$($(package)_file_name) && \
@@ -44,7 +39,6 @@ endef
 
 define $(package)_preprocess_cmds
   sed -i "s|GCC_DIR|LLVM_CLANG_DIR|g" cctools2odcctools/extract.sh && \
-  sed -i "s|llvmgcc42-2336.1|clang+llvm-3.2-x86-linux-ubuntu-12.04|g" cctools2odcctools/extract.sh && \
   sed -i "s|/llvmCore/include/llvm-c|/include/llvm-c \$$$${LLVM_CLANG_DIR}/include/llvm |" cctools2odcctools/extract.sh && \
   sed -i "s|fAC_INIT|AC_INIT|" cctools2odcctools/files/configure.ac && \
   sed -i 's/\# Dynamically linked LTO/\t ;\&\n\t linux*)\n# Dynamically linked LTO/' cctools2odcctools/files/configure.ac && \
@@ -62,7 +56,6 @@ endef
 
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install && \
-  cd ../../clang+llvm-3.2-x86-linux-ubuntu-12.04 && \
   mkdir -p $($(package)_staging_prefix_dir)/lib/clang/3.2/include && \
   mkdir -p $($(package)_staging_prefix_dir)/bin && \
   cp -P bin/clang bin/clang++ $($(package)_staging_prefix_dir)/bin/ &&\
